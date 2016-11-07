186380559c20bec36a49b83a06993b3326558e1b
Harpreet <preity.055@gmail.com>
2016-11-07 14:32:57 +0530

    my first commit

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views/pages/'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.get('/bad', function(request, response) {
+  response.sendFile(__dirname + '/views/pages/HTMLBadWay.html');
+});
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/right', function(request, response) {
+  response.sendFile(__dirname + '/views/pages/HTMLRightWay.html');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+
 
 
